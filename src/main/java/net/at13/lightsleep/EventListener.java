6ed62fc18e60f811238e6cf6b9d8a3a52f2ad528
0AT13 <anton13griogruk@gnail.com>
2021-09-21 13:42:07 +0300

    Changed message Added config file

--- src/main/java/net/at13/lightsleep/EventListener.java
+++ src/main/java/net/at13/lightsleep/EventListener.java
@@ -44,9 +44,13 @@ public class EventListener implements Listener {
 
             public void run() {
                 if(player.isSleeping()) {
-                    Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + player.getName() + "" +
+                    Bukkit.broadcastMessage(
+                            ChatColor.LIGHT_PURPLE + player.getName() + "" +
                             ChatColor.WHITE + " is sleeping");
-                    Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "[" + Bukkit.getServer().getName() + "]" + ChatColor.BLUE + "[LightSleep] " + ChatColor.LIGHT_PURPLE + "" + (int)sleepingPlayersPercent + "" + "(" + LightSleep.percentNeedToSleep + ")/100%" +
+
+                    Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "[" + Bukkit.getServer().getName() + "]" +
+                            ChatColor.BLUE + "[LightSleep] " +
+                            ChatColor.LIGHT_PURPLE + "" + (int)sleepingPlayersPercent + "" + "(" + LightSleep.percentNeedToSleep + ")/100%" +
                             ChatColor.WHITE + " of players is sleeping");
                 }
             }
@@ -70,7 +74,10 @@ public class EventListener implements Listener {
         Player player = event.getPlayer();
         UUID playerUUID = player.getUniqueId();
 
-        Bukkit.broadcastMessage(ChatColor.LIGHT_PURPLE + "[" + Bukkit.getServer().getName() + "]" + ChatColor.BLUE + "[LightSleep] " + ChatColor.LIGHT_PURPLE + player.getName() + "" +
+        Bukkit.broadcastMessage(
+                ChatColor.LIGHT_PURPLE + "[" + Bukkit.getServer().getName() + "]" +
+                ChatColor.BLUE + "[LightSleep] " +
+                ChatColor.LIGHT_PURPLE + player.getName() + "" +
                 ChatColor.WHITE + " woke up");
 
         sleepingPlayers.removeSleepingState(playerUUID);
